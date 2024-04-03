7f5646be37592a1287c2a4a58a7e85e0a33e3e1a
madoka <mnogaki@my.bcit.ca>
2024-04-03 16:36:12 -0700

    install header in each page

--- pages/typography.js
+++ pages/typography.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
       <Head>
         <title>Typography</title>
       </Head>
+      <Header />
       <main>
         Typography Page
       </main>
