f804cea390748d9e1a08943df28626968b88c0c6
Yae-Won Lee <ylee384@my.bcit.ca>
2024-04-03 18:42:42 -0700

    edit header to highlight active page

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,17 +1,28 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 import styles from './Header.module.css';
 import Link from 'next/link';
 
 export default function Header() {
-    return(
+    const router = useRouter();
+    const [active, setActive] = useState(false);
+
+    return (
         <header className={styles.headerContainer}>
             <div className={styles.logoContainer}>Logo here</div>
             <div className={styles.menuContainer}>
-                <Link href='/logo'>Logo</Link>
-                <Link href='/graphics'>Graphics</Link>
-                <Link href='/colours'>Colours</Link>
-                <Link href='/typography'>Typography</Link>
-                <Link href='/references'>References</Link>
+                <Link href='/logo'><div className={router.pathname === '/logo' ? styles.active : styles.link}>Logo</div> </Link>
+
+                <Link href='/graphics'><div className={router.pathname === '/graphics' ? styles.active : styles.link}>Graphics</div> </Link>
+
+                <Link href='/colours'><div className={router.pathname === '/colours' ? styles.active : styles.link}>Colours</div></Link>
+
+                <Link href='/typography'><div className={router.pathname === '/typography' ? styles.active : styles.link}>Typography</div></Link>
+
+                <Link href='/references'><div className={router.pathname === '/references' ? styles.active : styles.link}>References</div></Link>
             </div>
+
         </header>
     )
 }
+
