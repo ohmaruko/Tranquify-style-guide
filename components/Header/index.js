b1e0702fdee49feb1d00e9c1b60048b323973a27
Yae-Won Lee <ylee384@my.bcit.ca>
2024-04-03 19:03:02 -0700

    add logo + text svg to header component

--- components/Header/index.js
+++ components/Header/index.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { useRouter } from 'next/router';
 import styles from './Header.module.css';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Header() {
     const router = useRouter();
@@ -9,7 +10,17 @@ export default function Header() {
 
     return (
         <header className={styles.headerContainer}>
-            <div className={styles.logoContainer}>Logo here</div>
+            {/* <div className={styles.logoContainer}>
+                <Image src='/images/logo-48px.svg' alt='logo' width={60} height={60} />
+                <Image src='/images/Tranquify-logo-text.svg' alt='tranquify' width={90} height={90} />
+            </div> */}
+             <Link href="/">
+                <div className={styles.logoContainer}>
+                    <Image src='/images/logo-48px.svg' alt='logo' width={60} height={60} />
+                    <Image src='/images/Tranquify-logo-text.svg' alt='tranquify' width={90} height={90} />
+                </div>
+            </Link>
+
             <div className={styles.menuContainer}>
                 <Link href='/logo'><div className={router.pathname === '/logo' ? styles.active : styles.link}>Logo</div> </Link>
 
