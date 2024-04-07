d78758a8e0a12b4dd164320aaa6d94a4d3e31ccd
“Crystal <hcheung109@my.bcit.ca>
2024-04-06 21:19:25 -0700

    update typography page

--- pages/typography.js
+++ pages/typography.js
@@ -8,10 +8,14 @@ export default function Home() {
     <>
       <Head>
         <title>Typography</title>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <Header />
       <main className={styles.main}>
-        <h1>Typography</h1>
+        <h1 className={styles.typographyTitle}>Typography</h1>
         <div className={styles.logoFont}>
           <div className={styles.quicksand}>
             <h2>Quicksand</h2>
@@ -20,7 +24,7 @@ export default function Home() {
             <p>1234567890</p>
           </div>
           <div className={styles.quicksandText}>
-            Quicksand is chosen as the logo font because of its friendly style. The rounded style of the font compliments well with our logo and mascots, which also have a smooth and rounded shapes. Its smooth curves and clean look convey a sense of calmness, inclusiveness, and approachability, which align well with our mood tracking and meditation app.  
+            <span className={styles.boldText}>Quicksand</span> is chosen as the logo font because of its friendly style. The rounded style of the font compliments well with our logo and mascots, which also have a smooth and rounded shapes. Its smooth curves and clean look convey a sense of calmness, inclusiveness, and approachability, which align well with our mood tracking and meditation app.  
           </div>
         </div>
         <div className={styles.logoFontUsage}>
@@ -32,10 +36,10 @@ export default function Home() {
               <path d="M58.5314 27.6268C56.7583 27.6268 54.7255 27.2462 52.3971 26.3283C51.824 26.1 51.5419 25.4507 51.7658 24.8776C51.9941 24.3045 52.6389 24.0179 53.2165 24.2462C57.4523 25.9164 63.2106 26.5343 66.618 19.997C66.9046 19.4462 67.5807 19.2358 68.127 19.5224C68.6732 19.8089 68.8882 20.4851 68.6016 21.0313C67.1688 23.7851 64.1553 27.6313 58.527 27.6313L58.5314 27.6268Z" fill="white"/>
               <path d="M37.1685 48.9045C29.4849 48.9045 22.6835 47.3955 17.7984 44.4627C17.27 44.1448 17.0954 43.4552 17.4133 42.9269C17.7312 42.3985 18.4208 42.2239 18.9491 42.5418C36.4969 53.0687 69.9447 42.7522 85.0118 26.9194C85.4372 26.4716 86.1447 26.4537 86.5924 26.8791C87.0402 27.3045 87.0581 28.0119 86.6327 28.4597C78.0044 37.5224 65.082 44.4134 51.1745 47.3687C46.3432 48.394 41.6059 48.9 37.1641 48.9L37.1685 48.9045Z" fill="white"/>
             </svg>
-            <h2>Tranquify</h2>
+            <h2 className={styles.logoName}>Tranquify</h2>
           </div>
           <div className={styles.logoStyle}>
-            <h2>Bold 36pts</h2>
+            <p>Bold 36pts</p>
           </div>
         </div>
 
@@ -47,29 +51,37 @@ export default function Home() {
             <p>1234567890</p>
           </div>
             <div className={styles.quicksandText}>
-              DM Sans is chosen as the body font because of its great readability. Its clear letter forms allows easy-reading and stress-free experience, crucial for users engaging with mood tracking and meditation content. DM Sans also provides a comfortable reading experience across different devices, enhancing the usability of our app.
+              <span className={styles.boldText}>DM Sans</span> is chosen as the body font because of its great readability. Its clear letter forms allows easy-reading and stress-free experience, crucial for users engaging with mood tracking and meditation content. DM Sans also provides a comfortable reading experience across different devices, enhancing the usability of our app.
             </div>
         </div>
         <div className={styles.bodyFontUsage}>
           <div className={styles.fontTable}>
-            <div>H1</div>
-            <div>Heading</div>
-            <div className={styles.gridItem}>Regular 32pts</div>
-            <div>H2</div>
-            <div>Subheading</div>
-            <div>Bold 20pts</div>
-            <div>Paragraph</div>
-            <div>Bold Body</div>
-            <div>Bold 16pts</div>
-            <div></div>
-            <div>Body</div>
-            <div>Regular 16pts</div>
-            <div></div>
-            <div>Small Body</div>
-            <div>Regular 12pts</div>
-            <div>Links</div>
-            <div className={styles.link}>Continue as guest</div>
-            <div>Bold 16pts</div>
+            <div className={styles.fontRow}>
+              <div>H1</div>
+              <div className={styles.heading}>Heading</div>
+              <div className={styles.gridItem}>Regular 32pts</div>
+            </div>
+            <div className={styles.fontRow}>
+              <div>H2</div>
+              <div className={`${styles.boldText} ${styles.subheading}`}>Subheading</div>
+              <div className={styles.boldText}>Bold 20pts</div>
+            </div>
+            <div className={styles.fontRow}>
+              <div>Paragraph</div>
+              <div className={styles.boldText}>Bold Body</div>
+              <div className={styles.boldText}>Bold 16pts</div>
+              <div></div>
+              <div>Body</div>
+              <div>Regular 16pts</div>
+              <div></div>
+              <div className={styles.smallBody}>Small Body</div>
+              <div>Regular 12pts</div>
+            </div>
+            <div className={styles.fontRow}>
+              <div>Links</div>
+              <div className={styles.link}>Continue as guest</div>
+              <div className={styles.boldText}>Bold 16pts</div>
+            </div>
           </div>
         </div>
       </main>
