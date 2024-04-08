9f711bb38cd6bb75b65fc1bbc42e29303c70906f
Yae-Won Lee <ylee384@my.bcit.ca>
2024-04-07 23:50:59 -0700

    edit spelling and text-align

--- pages/graphics.js
+++ pages/graphics.js
@@ -29,10 +29,10 @@ export default function Home() {
             <p>Mascots</p>
           </div>
           <div className={styles.text}>
-            <p>Our five mascots represent different moods, 
-              each depicted in a unique color palette to maintain consistent tones aligned with our app's design. 
-              To enhance inclusivity and approachability, the mascots are intentionally designed with imperfect shapes, 
-              conveying the message that it's ok not to be perfect.
+            <p>Our five mascots represent different moods,
+              each depicted in a unique colour palette to maintain consistent tones aligned with our app's design.
+              To enhance inclusivity and approachability, the mascots are intentionally designed with imperfect shapes,
+              conveying the message that it's okay not to be perfect.
             </p>
           </div>
         </div>
@@ -40,9 +40,9 @@ export default function Home() {
           <div className={styles.graphics}>
             {
               mascots.map((e, index) => {
-                return(
-                      <Image src={e} width={80} height={100} alt={mascotsAltText[index]} />
-                  )
+                return (
+                  <Image src={e} width={80} height={100} alt={mascotsAltText[index]} />
+                )
               })
             }
           </div>
@@ -51,7 +51,7 @@ export default function Home() {
               colours.map((e) => {
                 return (
                   <div>
-                    <div className={styles.palette} style={{backgroundColor: e}}></div>
+                    <div className={styles.palette} style={{ backgroundColor: e }}></div>
                     <p>#A7BF87</p>
                   </div>
                 )
@@ -64,36 +64,36 @@ export default function Home() {
             <p>Icons</p>
           </div>
           <div className={styles.text}>
-            <p>Icons are utilized to improve visual communication with users, 
-              enabling them to intuitively locate navigations or buttons. 
-              Each icon is accompanied by label text, ensuring that users can read the label 
-              even if they are unsure of the icon's meaning. 
-              We use icons from <Link href="https://fonts.google.com/icons">Google</Link> 
+            <p>Icons are utilized to improve visual communication with users,
+              enabling them to intuitively locate navigations or buttons.
+              Each icon is accompanied by label text, ensuring that users can read the label
+              even if they are unsure of the icon's meaning.
+              We use icons from <Link href="https://fonts.google.com/icons">Google </Link>
               as they offer simplicity and align with user expectations for design.</p>
           </div>
         </div>
         <div className={styles.graphicsContainer}>
           <div className={styles.icons}>
             <p className={styles.iconText}>Navigation Bar</p>
-            <p className={styles.iconDescription}>Navigation bar icons represent different pages within the app, 
+            <p className={styles.iconDescription}>Navigation bar icons represent different pages within the app,
               making it easier for users to navigate to each page.</p>
             <Image src={'/images/navbar.svg'} width={430} height={94} alt="Home Icon" />
           </div>
           <div className={styles.icons}>
             <p className={styles.iconText}>Mood Tracker Questions</p>
             <p className={styles.iconDescription}>
-              On the mood tracker page, users are presented with a series of questions to answer. 
-              These questions feature icon options that users can quickly select to indicate 
+              On the Mood Tracker page, users are presented with a series of questions to answer.
+              These questions feature icon options that users can quickly select to indicate
               their mood, stress level, and other parameters.</p>
             <div className={styles.graphics}>
               {
                 moods.map((e, index) => {
-                  return(
-                      <div className={styles.moodIcon}>
-                        <Image src={e} width={60} height={60} alt={moodsAltText[index]} />
-                        <p>{moodsText[index]}</p>
-                      </div>
-                    )
+                  return (
+                    <div className={styles.moodIcon}>
+                      <Image src={e} width={60} height={60} alt={moodsAltText[index]} />
+                      <p>{moodsText[index]}</p>
+                    </div>
+                  )
                 })
               }
             </div>
@@ -103,8 +103,8 @@ export default function Home() {
           <div className={styles.icons}>
             <p className={styles.iconText}>Setting Menu</p>
             <p className={styles.iconDescription}>
-              The settings page contains eight setting menus, 
-              each accompanied by icons on their respective buttons. 
+              The Settings page contains eight setting menus,
+              each accompanied by icons on their respective buttons.
               These icons help users easily locate the setting option they desire.
             </p>
             <div className={styles.iconsItemsContainer}>
